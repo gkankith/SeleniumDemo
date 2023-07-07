43be9ef411ebd4ad45a7360c3c0e932f4e443b2c
gkankith <138243968+gkankith@users.noreply.github.com>
2023-07-07 11:54:50 +0530

    Update CustomerModule.java

--- src/com/acitime/testscript/CustomerModule.java
+++ src/com/acitime/testscript/CustomerModule.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import com.actitime.generic.BaseClass;
 @Listeners(com.actitime.generic.ListenerImplementation.class)
 public class CustomerModule extends BaseClass {
-	
+	//pullback
 	@Test
 	public void testcreateCustomer() {
 		Reporter.log("create",true);
